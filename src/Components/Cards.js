d562dd4f84546af7b386ccf5d1463625d8071f89
Gedeon IRAFASHA <irafasha.jedy12@gmail.com>
2023-03-09 19:52:34 +0200

    addind create products and filter functionalities (#9)

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Card from './Card';
 import DropDown from './DropDown';
 import Modal from './Modal';
@@ -7,31 +7,55 @@ import Modal from './Modal';
 export default function Cards() {
     const [open,setOpen]=useState(false);
     const [openModal,setOpenModal]=useState(false)
+    const [category,setCategory]=useState('')
+    const [products,setProducts]=useState([]);
+    const [searchWord,setSearchWord]=useState('');
 
+    const handleFetch=async()=>{
+        await fetch("http://localhost:3000/products")
+        .then(data=>data.json())
+        .then(response=>setProducts(response));
+    }
+
+    const Filter = products.filter((product)=>{
+        return product.name.toLowerCase().includes(searchWord.toLowerCase()) && product.Category.toLowerCase().includes(category.toLowerCase());
+    })
+
+    useEffect(()=>{
+        handleFetch();
+    },[])
+
+console.log(Filter)
   return (
     <><div className="lg:flex justify-between px-8 sm:block py-20">
           <div className="mb-2 lg:w-64 sm:w-full">
               <input type="text"
                   id="searchWord"
+                  value={searchWord}
+                  onChange={(e)=>setSearchWord(e.target.value)}
                   className="bg-gray-50 shadow text-sm rounded-xs outline-none focus:ring-yellow-300 focus:border-yellow-300 block w-full p-2.5"
                   placeholder="Search for products" required />
           </div>
-          <button onClick={() => setOpen(!open)} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="w-48 justify-between bg-gray-50 hover:bg-gray-100 focus:ring-1 focus:outline-none font-medium rounded-xs text-sm px-4 py-1 h-10 text-center inline-flex items-center " type="button">Filter by Region<svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+          <button onClick={() => setOpen(!open)} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="w-48 justify-between bg-gray-50 hover:bg-gray-100 focus:ring-1 focus:outline-none font-medium rounded-xs text-sm px-4 py-1 h-10 text-center inline-flex items-center " type="button">{category===''?(<>Filter by Region</>):(category)}<svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
           {open &&
-                <DropDown/>
+                <DropDown setCategory={setCategory}/>
             }
         </div>
         <div className='flex justify-between px-8'>
-            <h3 className="text-xl mb-2 text-gray-900 font-bold">Products(1)</h3>
+            <h3 className="text-xl mb-2 text-gray-900 font-bold">Products</h3>
             <button onClick={()=>setOpenModal(true)} data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block mb-2 text-white bg-black hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xs text-sm px-5 py-2.5 text-center" type="button">
                 Add new product
             </button>
         </div>
-        {openModal&& <Modal setOpenModal={setOpenModal}/>}
+        {openModal&& <Modal setOpenModal={setOpenModal} handleFetch={handleFetch}/>}
         <hr/>
-        <div className="lg:grid lg:grid-cols-4 lg:gap-3 px-8 py-2  my-4 sm:block">
-            <Card />
-        </div>
+        {Filter.length===0?(<p className='p-10 text-center'>No Result found!</p>):(
+            <div className="lg:grid lg:grid-cols-4 lg:gap-3 px-8 py-2  my-4 sm:block">
+                {Filter.map((product)=>{
+                    return <Card key={product.id} image={product.image} category={product.Category} price={product.price} description={product.description}/>
+                })}
+            </div>
+        )}
 
         
         </>
