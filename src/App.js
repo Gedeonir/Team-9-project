99667777ef6943dcf77d1e56c373372442a79c49
adeline <adelinemug6@gmail.com>
2023-03-09 10:55:16 +0200

    routing configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
+import { Routes,Route } from 'react-router-dom';
 import './App.css';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Routes>
+        <Route path='/home' element={<Navigation/>}>
+
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
