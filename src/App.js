11976f518737682963b30c7cfb0e546d947e579c
adeline <adelinemug6@gmail.com>
2023-03-09 11:26:08 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
   return (
+<<<<<<< HEAD
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
+=======
+    
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path='/home' element={<Navigation/>}>
+
+        </Route>
+      </Routes>
+    </div>
+>>>>>>> 0d08eb0 (adding files)
   );
 }
 
