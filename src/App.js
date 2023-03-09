145297ff0abb8d1234f3de76fe8f0d8b7c4daaa7
Mucyobrian123 <mucyobrian7@gmail.com>
2023-03-09 16:28:29 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import { Routes,Route } from 'react-router';
 import Navigation from './Components/Navigation';
 import './App.css';
-
+import Footer from './Components/footer';
 function App() {
   return ( 
     <div>
       <Routes>
         <Route path='/home' element={<Navigation/>}>
-
+        
         </Route>
+        <Route path='/Footer' element={<Footer/>}/>
       </Routes>
     </div>
   );
---
+++ src/Components/footer.js
@@ -0,0 +1,40 @@
+
+
+function Footer (){
+return (
+    <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900 absolute left-0 right-0 bottom-0">
+    
+        <div class="sm:flex sm:items-center sm:justify-between ">
+        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+            
+           
+        </a>
+        
+        
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-black-500 sm:mb-0 dark:text-gray-400">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">Contact</a>
+            </li>
+        </ul>
+    </div>
+    
+    {/* <div class="bg-gray-900"> */}
+<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <span class="block text-sm text-black-500 sm:text-center dark:text-gray-400 ">© <a href="https://electronics.com/" class="hover:underline">Electronics</a>. All Rights Reserved.
+    </span>
+    {/* </div> */}
+    
+    </footer>
+
+)}
+export default Footer;
+
