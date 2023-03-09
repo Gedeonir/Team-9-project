e8678f53f294d76cd08622b678008eacb5dc1b2b
AdelineA <79213654+AdelineA@users.noreply.github.com>
2023-03-09 11:45:56 +0200

    adding files (#5)
    * adding files
    
    * routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
+import { Routes,Route } from 'react-router';
+import Navigation from './Components/Navigation';
 import './App.css';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return ( 
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path='/home' element={<Navigation/>}>
+
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
