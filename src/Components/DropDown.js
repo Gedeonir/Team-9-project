64db841afaca659a1ff22fe7162d1d40e58823bc
Gedeonir <irafasha.jedy12@gmail.com>
2023-03-10 09:09:50 +0200

    Fixes

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function DropDown(props) {
   return (
-    <div id="dropdown" className="z-10 absolute right-8 w-48 top-40 bg-white divide-y  divide-gray-100 rounded-lg shadow">
+    <div id="dropdown" className="z-10 absolute right-16 w-48 mt-12 bg-white divide-y  divide-gray-100 rounded-lg shadow">
         <ul className="py-2 text-sm " aria-labelledby="dropdownDefaultButton">
-            <li className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50" onClick={()=>props.setCategory('Laptops')}>
+            <li className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50" onClick={()=>{props.setCategory('Laptops');props.setOpen(false)}}>
                 Laptops
             </li>
-            <li onClick={()=>props.setCategory('Desktops')} className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50">
+            <li onClick={()=>{props.setCategory('Desktops');props.setOpen(false)}} className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50">
                 Desktops
             </li>
-            <li onClick={()=>props.setCategory('Speakers')} className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50">
+            <li onClick={()=>{props.setCategory('Speakers');props.setOpen(false)}} className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50">
                 Speakers
             </li>
-            <li onClick={()=>props.setCategory('Telephone')} className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50">
+            <li onClick={()=>{props.setCategory('Telephone');props.setOpen(false)}} className="px-3 py-2 w-full text-left cursor-pointer hover:bg-gray-50">
                 Telephone
             </li>
-            <li onClick={()=>props.setCategory('TV')} className="px-3 py-2 w-full text-left">
+            <li onClick={()=>{props.setCategory('TV');props.setOpen(false)}} className="px-3 py-2 w-full text-left">
                 TV
             </li>
         </ul>
